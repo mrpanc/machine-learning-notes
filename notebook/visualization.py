0abddc431125388c8e2548751842e17dc5b1481d
mrpanch <mrpancheng@126.com>
2018-01-30 19:43:54 +0800

    上传博文“探索神经网络学习率下降问题” 相关代码及图片

---
+++ notebook/visualization.py
@@ -0,0 +1,60 @@
+# coding: utf-8
+
+# import matplotlib as mpl
+# mpl.use('TkAgg')
+import matplotlib.pyplot as plt
+from network1 import sigmoid, sigmoid_prime
+import numpy as np
+
+
+def save_imgae(filename, fig, dpi=300):
+    """保存图片"""
+    fig.savefig(filename, dpi=dpi)
+
+
+def plot_training_cost(training_cost, axe=None):
+    """绘制训练损失随Epoch变化的曲线"""
+    if axe is None:
+        axe = plt.subplot(1, 1, 1)
+    axe.set_xlabel('Epoch')
+    axe.set_ylabel('Cost')
+    axe.plot(training_cost)
+    axe.grid(True)
+
+
+def plot_output(outputs, axe=None):
+    """绘制输出随epoch变化的曲线"""
+    if axe is None:
+        axe = plt.subplot(1, 1, 1)
+    axe.set_xlabel('Epoch')
+    axe.set_ylabel('Output')
+    axe.plot(outputs)
+    axe.grid(True)
+
+
+def plot_sigmoid(x=np.linspace(-10, 10, 1000), axe=None):
+    """绘制sigmoid函数图像"""
+    if axe is None:
+        axe = plt.subplot(1, 1, 1)
+    axe.set_xlabel('z')
+    axe.set_ylabel('Value of sigmoid function')
+    axe.set_title('Figure of sigmoid function')
+    axe.plot(x, sigmoid(x))
+    axe.grid(True)
+
+
+def plot_sigmoid_prime(x=np.linspace(-10, 10, 1000), axe=None):
+    """绘制sigmoid_prime函数图像"""
+    if axe is None:
+        axe = plt.subplot(1, 1, 1)
+    axe.set_xlabel('z')
+    axe.set_ylabel('Value of sigmoid function')
+    axe.set_title('Figure of sigmoid prime function')
+    axe.plot(x, sigmoid_prime(x))
+    axe.grid(True)
+
+
+def plot_mnist_image(matrix):
+    """展示MNIST数据集中的数字"""
+    assert(len(matrix) == 784)
+    plt.imshow(np.reshape(matrix, [28, 28]))
